811e9d33441773c13567099cfbb3f5eaaf22e65e
mathieu bultel <mat.bultel@gmail.com>
2024-12-24 11:26:30 +0100

    Re-org go modules

---
+++ vmware_migration_kit/plugins/module_utils/vmware/vmware.go
@@ -0,0 +1,97 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright 2024 Red Hat, Inc.
+ *
+ */
+
+package vmware
+
+import (
+	"context"
+	"crypto/sha1"
+	"crypto/tls"
+	"errors"
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/vmware/govmomi/object"
+	"github.com/vmware/govmomi/vim25/mo"
+	"github.com/vmware/govmomi/vim25/types"
+)
+
+var logger *log.Logger
+var logFile string = "/tmp/osm-nbdkit.log"
+
+func init() {
+	logFile, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatalf("Failed to open log file: %v", err)
+	}
+	logger = log.New(logFile, "osm-nbdkit: ", log.LstdFlags|log.Lshortfile)
+}
+
+func ProcessUrl(u *url.URL, user string, password string) {
+	if user != "" {
+		u.User = url.UserPassword(user, password)
+	}
+}
+
+func GetThumbprint(host string, port string) (string, error) {
+	config := tls.Config{
+		InsecureSkipVerify: true,
+	}
+	if port == "" {
+		port = "443"
+	}
+
+	conn, err := tls.Dial("tcp", fmt.Sprintf("%s:%s", host, port), &config)
+	if err != nil {
+		return "", err
+	}
+	defer conn.Close()
+
+	if len(conn.ConnectionState().PeerCertificates) == 0 {
+		logger.Printf("No certificates found")
+		return "", errors.New("no certificates found")
+	}
+
+	certificate := conn.ConnectionState().PeerCertificates[0]
+	sha1Bytes := sha1.Sum(certificate.Raw)
+
+	thumbprint := make([]string, len(sha1Bytes))
+	for i, b := range sha1Bytes {
+		thumbprint[i] = fmt.Sprintf("%02X", b)
+	}
+
+	return strings.Join(thumbprint, ":"), nil
+}
+
+func GetDiskKey(ctx context.Context, vm *object.VirtualMachine) ([]int32, error) {
+	var diskKeys []int32
+	var vmProperties mo.VirtualMachine
+	err := vm.Properties(ctx, vm.Reference(), []string{"config.hardware.device"}, &vmProperties)
+	if err != nil {
+		logger.Printf("Failed to retrieve VM properties: %v", err)
+		return nil, err
+	}
+	for _, device := range vmProperties.Config.Hardware.Device {
+		if virtualDisk, ok := device.(*types.VirtualDisk); ok {
+			diskKeys = append(diskKeys, virtualDisk.VirtualDevice.Key)
+		}
+	}
+	return diskKeys, nil
+}
---
+++ vmware_migration_kit/plugins/modules/import_image/import_image.go
@@ -0,0 +1,120 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright 2024 Red Hat, Inc.
+ *
+ */
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	moduleutils "vmware_migration_kit/vmware_migration_kit/plugins/module_utils"
+
+	"github.com/gophercloud/gophercloud"
+	"github.com/gophercloud/gophercloud/openstack"
+	"github.com/gophercloud/gophercloud/openstack/imageservice/v2/imagedata"
+	"github.com/gophercloud/gophercloud/openstack/imageservice/v2/images"
+	"github.com/gophercloud/utils/openstack/clientconfig"
+)
+
+var args struct {
+	Name     string `json:"name"`
+	DiskPath string `json:"disk_path"`
+}
+
+var logger *log.Logger
+var logFile string = "/tmp/osm-import-volume-as-image.log"
+
+func init() {
+	logFile, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatalf("Failed to open log file: %v", err)
+	}
+	logger = log.New(logFile, "osm-image: ", log.LstdFlags|log.Lshortfile)
+}
+
+func UploadImage(provider *gophercloud.ProviderClient, imageName, diskPath string) (string, error) {
+	client, err := openstack.NewImageServiceV2(provider, gophercloud.EndpointOpts{
+		Region: os.Getenv("OS_REGION_NAME"),
+	})
+	if err != nil {
+		return "", fmt.Errorf("failed to create image service client: %v", err)
+	}
+
+	createOpts := images.CreateOpts{
+		Name:            imageName,
+		DiskFormat:      "qcow2",
+		ContainerFormat: "bare",
+	}
+
+	image, err := images.Create(client, createOpts).Extract()
+	if err != nil {
+		return "", fmt.Errorf("failed to create image: %v", err)
+	}
+
+	file, err := os.Open(diskPath)
+	if err != nil {
+		return "", fmt.Errorf("failed to open disk file: %v", err)
+	}
+	defer file.Close()
+
+	err = imagedata.Upload(client, image.ID, file).ExtractErr()
+	if err != nil {
+		return "", fmt.Errorf("failed to upload image: %v", err)
+	}
+
+	return image.ID, nil
+}
+
+func main() {
+	var response moduleutils.Response
+
+	if len(os.Args) != 2 {
+		response.Msg = "No argument file provided"
+		moduleutils.FailJson(response)
+	}
+
+	argsFile := os.Args[1]
+	argsData, err := os.ReadFile(argsFile)
+	if err != nil {
+		response.Msg = fmt.Sprintf("Failed to read argument file: %v", err)
+		moduleutils.FailJson(response)
+	}
+
+	if err := json.Unmarshal(argsData, &args); err != nil {
+		response.Msg = fmt.Sprintf("Failed to parse argument file: %v", err)
+		moduleutils.FailJson(response)
+	}
+	opts, err := clientconfig.AuthOptions(nil)
+	if err != nil {
+		response.Msg = fmt.Sprintf("Failed to get auth options: %v", err)
+		moduleutils.FailJson(response)
+	}
+	provider, err := openstack.AuthenticatedClient(*opts)
+	if err != nil {
+		response.Msg = fmt.Sprintf("Failed to authenticate: %v", err)
+		moduleutils.FailJson(response)
+	}
+	imageID, err := UploadImage(provider, args.Name, args.DiskPath)
+	if err != nil {
+		response.Msg = fmt.Sprintf("Failed to upload image: %v", err)
+		moduleutils.FailJson(response)
+	}
+
+	response.ID = imageID
+	response.Msg = fmt.Sprintf("Image created successfully: %s", imageID)
+	moduleutils.ExitJson(response)
+}
