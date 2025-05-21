d2a8951cf0da00973fb5595b3029ec82db984022
Roberto Alfieri <ralfieri@redhat.com>
2025-05-21 16:18:00 +0200

    Rely on CentOS Stream 10 minimal as AEE base image
    Since the release cycle of Fedora is too short (6 months, with 2
    releases officially supported), we're switching our container image to
    CentOS Stream 10 which guarantee the right balance between stability and
    right features.
    
    To ease this transition, we switch the python version used for tests to
    3.12 which is available on both Fedora (41/42) and CentOS Stream 10
    
    Signed-off-by: Roberto Alfieri <ralfieri@redhat.com>

--- aee/build.sh
+++ aee/build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-img_registry=quay.io/os-migrate/vmware-migration-kit:latest
 tag_name=vmware-migration-kit
 
 ansible-builder build --tag $tag_name
-podman push localhost/$tag_name $img_registry
+rm -fr "$(pwd)/context/"
