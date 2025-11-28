60c5889a178258cacf977c8fee3de001122871c4
mathieu bultel <mat.bultel@gmail.com>
2025-11-28 15:08:13 +0100

    Add warn module and fix ansible sanity and lint

--- plugins/modules/warn.py
+++ plugins/modules/warn.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python
-# Copyright 2025 Red Hat, Inc.
-# All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# GNU General Public License v3.0+
+# (c) 2025, Your Name <you@example.com>
+# SPDX-License-Identifier: GPL-3.0-or-later
 
-from ansible.module_utils.basic import AnsibleModule
-import sys
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <https://www.gnu.org/licenses/>.
 
 DOCUMENTATION = r'''
 ---
 module: warn
 short_description: Print a warning without failing
-version_added: "2.1.3"
-author: "Your Name"
+version_added: "2.0.0"
+author: "Mathieu Bultel (@matbu)"
 description:
   - Print a warning message in Ansible output without failing the play.
 options:
@@ -48,6 +49,9 @@ message:
   type: str
   returned: always
 '''
+
+from ansible.module_utils.basic import AnsibleModule
+import sys
 
 
 def print_warning(message):
