ca43a238b2049b478a4812305e7750f0aa4987f7
Fernando Diaz <fdiazbra@redhat.com>
2026-01-12 14:15:47 +0100

    Keep only the bindep in root folder and adapt the build for the path of bindep

--- aee/build.sh
+++ aee/build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
+set -e
 
 tag_name=vmware-migration-kit
+EE_DIR="$(dirname "$0")"
+cd "$EE_DIR"
 
-ansible-builder build --tag $tag_name
-rm -fr "$(pwd)/context/"
+ansible-builder build --tag "$tag_name"
+
+rm -rf "$EE_DIR/context/"
