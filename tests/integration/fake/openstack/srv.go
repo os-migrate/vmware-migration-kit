9c77e6e2a1778e061a53b8feae304c29ff0997f8
mathieu bultel <mat.bultel@gmail.com>
2026-02-04 22:07:10 +0100

    Fix copyright date

--- tests/integration/fake/openstack/srv.go
+++ tests/integration/fake/openstack/srv.go
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * Copyright 2026 Red Hat, Inc.
+ * Copyright 2025 Red Hat, Inc.
  *
  */
 package main
